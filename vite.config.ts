0e7aa2cada2f576d693697b81ea5901c539e5469
Bryant Wong <bryanthwong@gmail.com>
2026-09-02 21:21:45 -0400

    base path change

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  base: process.env.NODE_ENV === 'production'
-    ? '/return-the-stones/'
-    : '/'
+  base: 'return-the-stones'
 })
