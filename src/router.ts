2accb1b02136c0a4ebd99b9ed43c7b332fe7613e
Bryant Wong <bryanthwong@gmail.com>
2026-09-02 21:36:43 -0400

    attempt hash routing

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,6 @@
 import Home from '@/components/Home.vue'
 import SignOn from '@/components/SignOn.vue'
-import { createWebHistory, createRouter } from 'vue-router'
+import { createWebHashHistory, createRouter } from 'vue-router'
 
 const routes = [
   { path: '/', component: () => Home },
@@ -8,6 +8,6 @@ const routes = [
 ]
 
 export const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 })
