3c077bf61c504452acd17be4f7ad2d5136ce24c5
Bryant Wong <bryanthwong@gmail.com>
2026-09-03 07:21:36 -0400

    added 404

---
+++ src/components/Page404.vue
@@ -0,0 +1,7 @@
+<template>
+  <h2 class="font-bold">
+    404 Page not found
+  </h2>
+</template>
+
+<script setup lang="ts"></script>
--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,12 @@
 import Home from '@/components/Home.vue'
 import SignOn from '@/components/SignOn.vue'
+import Page404 from '@/components/Page404.vue'
 import { createWebHistory, createRouter } from 'vue-router'
 
 const routes = [
   { path: '/', component: Home },
   { path: '/sign-on/', component: SignOn },
+  { path: '/:pathMatch(.*)', component: Page404 },
 ]
 
 export const router = createRouter({
