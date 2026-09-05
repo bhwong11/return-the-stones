1c8be2d1df44d383bda647caa23d841fd6b249fe
Bryant Wong <bryanthwong@gmail.com>
2026-09-04 22:21:14 -0400

    center 404

--- src/components/Page404.vue
+++ src/components/Page404.vue
@@ -1,7 +1,9 @@
 <template>
-  <h2 class="font-bold">
-    404 Page not found
-  </h2>
+  <div class="flex justify-center">
+    <h2 class="font-bold pt-[2rem]">
+      404 Page not found
+    </h2>
+  </div>
 </template>
 
 <script setup lang="ts"></script>
--- src/components/SignOn.vue
+++ src/components/SignOn.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="flex flex-col items-center">
-    <h1 class="text-4xl font-bold tracking-tight pt-[2rem]">Want to support? Sign on here!</h1>
+    <h1 class="text-4xl font-bold tracking-tight pt-[2rem] text-center">Want to support? Sign on here!</h1>
     <a
-      class="text-blue-600 hover:underline font-medium pt-[2rem] text-2xl"
+      class="text-blue-600 hover:underline font-medium pt-[2rem] text-2xl text-center"
       href="https://docs.google.com/forms/d/e/1FAIpQLSe34h1Wm-8ntkyng5zn5h6tGqa1ol3xqMBL-GtKqv4Ypv3PSg/viewform?usp=sharing&ouid=111430167125586998476"
     >
       Sign On Here
