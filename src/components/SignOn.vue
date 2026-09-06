2a5afee5158a19fa196999f1b43a9d686f67c712
Bryant Wong <bryanthwong@gmail.com>
2026-09-06 19:23:29 -0400

    added logos for big buddle

--- src/components/SignOn.vue
+++ src/components/SignOn.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col items-center">
+  <div class="flex flex-col">
     <h1 class="text-4xl font-bold tracking-tight pt-[2rem] text-center">Want to support? Sign on here!</h1>
     <a
       class="text-blue-600 hover:underline font-medium pt-[2rem] text-2xl text-center"
@@ -7,7 +7,33 @@
     >
       Sign On Here
     </a>
+    <h3 class="pt-[2rem] font-bold text-xl flex justify-center">Signers</h3>
+    <div class="sign-on-wrapper flex flex-wrap justify-center px-[2rem]">
+      <div v-for="signOn in signOns" class="sign-on">
+        <div class=
+        "
+          sign-on-logo-wrapper
+          flex
+          flex-col
+          justify-center
+        "
+        >
+          <img :src="signOn.logo" :alt="signOn.organizationName" class="sign-on-logo">
+        </div>
+        <p class="text-center">{{ signOn.organizationName }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { signOns } from "@/utils/consts"
+</script>
+<style scoped>
+.sign-on {
+  width: 250px;
+}
+.sign-on-logo-wrapper {
+  height: 300px;
+}
+</style>
--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -1,7 +1,95 @@
-const signOnPath = '@/assets/sign-on-logos/'
-const images = [
+const signOnPath = '/src/assets/sign-on-logos/'
+export const signOns = [
   {
-    organizationName: '',
-    logo:`${signOnPath}arthur-Stewart.png`
-  }
+    organizationName: 'Christians for a Free Palestine, Catholic Caucus',
+    logo:`${signOnPath}Catholic-Caucus-Logo.png`,
+  },
+  {
+    organizationName: 'Palestinian Christian Alliance for Peace',
+    logo:`${signOnPath}pcap-logo-horizontal.jpeg`,
+  },
+    {
+    organizationName: 'FOSNA',
+    logo:``,
+  },
+  {
+    organizationName: 'Pax Christi International',
+    logo:`${signOnPath}PCI-LOGO-rvb.png`,
+  },
+  {
+    organizationName: 'Pax Christi USA',
+    logo:`${signOnPath}PC-USA-logo.jpeg`,
+  },
+  {
+    organizationName: 'Indiana Center for Middle East Peace',
+    logo:`${signOnPath}ICMEP-Logo-A-voice-of-conscience.png`,
+  },
+  {
+    organizationName: 'Justice4Palestine - Actions Enablement Network (J4P)',
+    logo:`${signOnPath}J4P-Justice-4-Palestine.png`,
+  },
+  {
+    organizationName: 'codepink',
+    logo:``,
+  },
+  {
+    organizationName: 'Dominican sister Grand Rapids',
+    logo:``,
+  },
+  {
+    organizationName: 'Peace Action Montgomery',
+    logo:`${signOnPath}montgomery-peace-action.png`,
+  },
+  {
+    organizationName: 'episcopal peace fellowship Palestine Justice Network',
+    logo:`${signOnPath}EPF-PJN-Logo.jpg`,
+  },
+  {
+    organizationName: 'Dorothy Day Catholic Worker',
+    logo:``,
+  },
+  {
+    organizationName: 'Christians for a Free Palestine ',
+    logo:`${signOnPath}CFP-Logo-Christians-for-a-Free-Palestine.png`,
+  },
+  {
+    organizationName: 'Asians4Palestine DMV',
+    logo:`${signOnPath}A4P.jpeg`,
+  },
+  {
+    organizationName: 'United Methodists for Kairos Response (UMKR)',
+    logo:`${signOnPath}UMKR_Logo_2024.jpg`,
+  },
+  {
+    organizationName: 'Palestinian Youth Movement',
+    logo:`${signOnPath}PYM.jpg`,
+  },
+  {
+    organizationName: 'member - Seekers Church',
+    logo:``,
+  },
+  {
+    organizationName: 'Mennonite Action',
+    logo:`${signOnPath}MA-logo.png`,
+  },
+  {
+    organizationName: 'The Open Door Community',
+    logo:`${signOnPath}open-door-logo.png`,
+  },
+  {
+    organizationName: 'Mennonite Action DMV',
+    logo:``,
+  },
+  {
+    organizationName: 'North Baltimore Mennonite Church',
+    logo:`${signOnPath}North-Baltimore-Mennonite-Church.png`,
+  },
+  {
+    organizationName: 'Pax Christi Metro DC-Baltmore',
+    logo:`${signOnPath}PCMDCB-Logo.jpg`,
+  },
+  {
+    organizationName: 'Maryland United for Peace & Justice',
+    logo:``,
+  },
 ]
